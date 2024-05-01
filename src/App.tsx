08048f4433756587f786fcb72d55cd81591937f1
MarlonCMSilva <marlon.conceicao.0308@gmail.com>
2024-05-01 13:13:53 -0300

    atualizando informações

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Teste from './Teste'
+import EstiloGlobal from './styte'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Teste />
+    </>
+  )
+}
+
+export default App

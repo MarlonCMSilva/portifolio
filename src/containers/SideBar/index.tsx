3a0d28c1827380a921a6389a5299bedcf4fffe9c
MarlonCMSilva <marlon.conceicao.0308@gmail.com>
2024-05-14 09:40:13 -0300

    Estilizando a SideBar

--- src/containers/SideBar/index.tsx
+++ src/containers/SideBar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descrição, BotaoTema, SideBarContainer } from './styles'
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/MarlonCMSilva.png" alt="" />
-    <Titulo fontSize={20}>Marlon Silva </Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Marlon Silva </Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        MarlonCMSilva
+      </Paragrafo>
+      <Descrição tipo="principal" fontSize={16}>
+        Desenvolvedor Full-Stack Java
+      </Descrição>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
